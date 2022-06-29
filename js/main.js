a961bb8369dc2ccd81df3bce8caa4cb8ea876f3b
Grumdrig87 <70903272+Grumdrig87@users.noreply.github.com>
2022-06-29 17:52:01 +0300

    29.06.22 17-51

--- js/main.js
+++ js/main.js
@@ -1,26 +1,11 @@
 jQuery(document).ready(function($) {
   
   //slider
-  const swiper = new Swiper('.swiper', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
-  const moreads = new Swiper('[data-moreads]', {
-    slidesPerView: 7,
-    freeMode: true,
-  });
-  const wheelSlide = new Swiper('[data-selectsSlide]', {
-    slidesPerView: 'auto',
-    freeMode: true,
-    spaceBetween: 10,
-    initialSlide : 2,
-    grid: {
-      rows: 2,
-      fill: 'row',
-    },
+
+  const wheelSlide = new Swiper('[data-kompl]', {
+    slidesPerView: 4,
+    spaceBetween: 23,
+    initialSlide : 1,
   });
   //adaptive
   if ($(window).width() < 994) {
